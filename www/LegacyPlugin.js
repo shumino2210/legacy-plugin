7912a25708e0508eb71971568926b9ebf62327c5
Carlos <callendelgua@na-at.com.mx>
2021-04-26 16:48:05 -0500

    Cambio en puente de javascript

--- www/LegacyPlugin.js
+++ www/LegacyPlugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'LegacyPlugin', 'coolMethod', [arg0]);
+exports.initProcess = function (arg0, success, error) {
+    exec(success, error, 'LegacyPlugin', 'initProcess', [arg0]);
 };
